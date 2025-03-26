3b7a50918f3f97ed695efc6d9ce6bcb520a5691d
Gian Marco Pablo Totocayo <gianmarcopablo588@gmail.com>
2025-03-25 23:27:34 -0500

    first commit

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [AsyncPipe, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+
+  router = inject(Router)
+
+  routes = routes.map(route => ({
+    path: route.path,
+    title: route.title ?? "Mapas en angular"
+  })).filter(route => route.path !== "**")
+
+  pageTitle$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    tap(event => console.log(event)),
+    map(event => event.url),
+    map(url => routes.find(route => `${route.path}` === url)?.title ?? "Mapas")
+  )
+}
